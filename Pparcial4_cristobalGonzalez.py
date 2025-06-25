d47a2df4664ada2610a6d4f1b9d6b17031098b10
CristobalGonzalez1 <cri.gonzalez5@duocuc.cl>
2025-06-25 12:16:19 -0400

    primera versión del código Pparcial4

---
+++ Pparcial4_cristobalGonzalez.py
@@ -0,0 +1,46 @@
+sg = 1
+
+entradaConce =[500]
+entradaPuentealto = [1300]
+entradaMuelleBaron = [100]
+entradaMuelleVergara = [50]
+
+
+def entrada_conce():
+    
+
+def entrada_puentealto():
+
+def entrada_BaronValparaiso():
+
+def entrada_VergaraViña():
+    
+
+def cerrar_programa():
+    print("Programa Terminado....")
+    sw = 0
+
+
+
+while sg == 1:
+    print("TOTEM AUTOSERVICIO GIRA ROCK AND CHILE IN CHILE")
+    print("1.- Comprar entrada a “los Fortificados” en Concepción.")
+    print("2.- Comprar entrada a “los Fortificados” en Puente Alto.")
+    print("3.- Comprar entrada a “los Fortificados” en Muelle Barón en Valparaíso.")
+    print("4.- Comprar entrada a “los Fortificados” en Muelle Vergara en Viña del Mar.")
+    print("5.- Salir.")
+    op = int(input("Seleccione una opcion: "))
+    if op == 1:
+
+    elif op == 2:
+
+    elif op == 3:
+
+    elif op == 4:
+
+    elif op == 5:
+
+
+    
+
+
